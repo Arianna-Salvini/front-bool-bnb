bf72bc8acab2bbdd9ba262ffa9c4604bfe3081a3
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-19 15:09:23 +0200

    add router, install axios add route for home and views component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './route.js'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+
---
+++ src/route.js
@@ -0,0 +1,17 @@
+import { createWebHistory, createRouter } from 'vue-router';
+
+import AppHome from './views/AppHome.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: AppHome
+        },
+
+    ]
+})
+
+export default router
