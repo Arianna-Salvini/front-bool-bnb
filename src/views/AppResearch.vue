d7903c62bf10c5bb39cac136f5805643e68d4bbe
alessiavozzo <a.vozzo95@gmail.com>
2024-06-20 17:52:31 +0200

    pass props to app research component

--- src/views/AppResearch.vue
+++ src/views/AppResearch.vue
@@ -12,18 +12,27 @@ export default {
     data() {
         return {
             state: state,
+            results: []
         }
     },
     methods: {
 
     },
     mounted() {
-
+        console.log(this.$route.query.results);
+        //initialize results as empty array and reassign results -> results are in json -> must convert back to object
+        this.results = JSON.parse(this.$route.query.results);
     }
 };
 </script>
 
 <template>
+
+    <!-- <p>{{ results }}</p> -->
+    <ul v-if="results.length != 0">
+        <li v-for="result in results">{{ result.title }}</li>
+    </ul>
+
 
 </template>
 
