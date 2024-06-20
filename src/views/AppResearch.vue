7c29b3745e4d112a4f7e153593db0e43adaaa4cc
alessiavozzo <a.vozzo95@gmail.com>
2024-06-20 23:58:44 +0200

    add research page layout + link to single apartment

--- src/views/AppResearch.vue
+++ src/views/AppResearch.vue
@@ -36,13 +36,58 @@ export default {
 <template>
 
     <section id="search-results">
+        <div class="container">
+            <div class="row g-4" v-if="results.length != 0">
+                <div v-for="result in results" class="col-6">
+
+                    <router-link :to="{ name: 'SingleApartment', params: { slug: result.slug } }"
+                        style="text-decoration: none;">
+                        <div class="card">
+                            <img v-if="result.image"
+                                :src="result.image.startsWith('http') ? result.image : state.base_api + '/storage/' + result.image"
+                                alt="result Image" class="card-img-top w-100" style="height: 350px;">
+                            <img v-else
+                                src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+                                alt="not-available" style="height: 350px;">
+                            <div class="card-body">
+                                <h3>{{ result.title }}</h3>
+                                <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    {{ result.address }}
+                                </p>
+                                <p class="card-text" v-if="result.description">
+                                    {{ result.description }}
+                                </p>
+
+                            </div>
+                        </div>
+                    </router-link>
+
+                </div>
+            </div>
+
+            <p v-else>No apartments found within the given range.</p>
+
+        </div>
 
     </section>
-    <ul v-if="results.length != 0">
-        <li v-for="result in results">{{ result.title }}</li>
-    </ul>
+
+
 
 
 </template>
 
-<style></style>
+<style scoped>
+#search-results {
+    padding: 2rem 0;
+}
+
+.card,
+img {
+    border-radius: 20px;
+}
+
+.card {
+    box-shadow: 0 0 12px 1px var(--color_grey_shadow);
+}
+</style>
