970c41190b2aa31c948c83bcd9ae204397c3d7c5
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-06-30 20:21:18 +0200

    Add style page not found in apartment

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,11 +5,38 @@ export default {
 </script>
 
 <template>
-    <div class="not-found">
-        not found
+    <div class="container">
+        <div class="not_found">
+            <img src="/img/no-apartments-illustration.png" alt="">
+            <p>No apartment found, sorry...</p>
+        </div>
     </div>
+
 </template>
 
 
 
-<style scoped></style>
+<style scoped>
+.container {
+    height: calc(100vh - 265px);
+
+    .not_found {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        img {
+            filter: grayscale();
+        }
+
+        p {
+            margin-bottom: 5rem;
+            font-size: 2rem;
+            color: var(--bnb-main);
+        }
+
+    }
+}
+</style>
